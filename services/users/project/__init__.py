95aa2b2ee0970733b9538e263f7f0ecf98fb90b5
Trung Tran <trungtran.chunml@gmail.com>
2019-09-04 11:01:47 +0900

    Set up docker

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 api = Api(app)
-app.config.from_object('project.config.DevelopmentConfig')
+app.config.from_object(os.getenv('APP_SETTINGS'))
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
