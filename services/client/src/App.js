1a315112805482160c4313567710915a711af7d4
Trung Tran <trungtran.chunml@gmail.com>
2019-10-27 15:53:43 +0900

    Refactor Form component

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -13,21 +13,11 @@ export default class App extends React.Component {
 
     this.state = {
       users: [],
-      username: '',
-      email: '',
       title: 'TrungTran',
-      formData: {
-        username: '',
-        email: '',
-        password: ''
-      },
       isAuthenticated: false
     };
 
-    this.addUser = this.addUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
-    this.handleFormChange = this.handleFormChange.bind(this);
+    this.loginUser = this.loginUser.bind(this);
     this.logoutUser = this.logoutUser.bind(this);
   }
 
@@ -47,82 +37,10 @@ export default class App extends React.Component {
       .catch(err => console.log(err));
   }
 
-  addUser(event) {
-    event.preventDefault();
-    const data = {
-      username: this.state.username,
-      email: this.state.email
-    };
-
-    fetch(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => {
-        this.getUsers();
-        this.setState({
-          username: '',
-          email: ''
-        })
-      })
-      .catch(err => console.log(err));
-  }
-
-  handleChange(event) {
-    const obj = {};
-    obj[event.target.name] = event.target.value;
-    this.setState(obj)
-  }
-
-  handleUserFormSubmit(e) {
-    e.preventDefault();
-    const formType = window.location.href.split('/').reverse()[0];
-    let data = {
-      email: this.state.formData.email,
-      password: this.state.formData.password
-    };
-    if (formType === 'register') {
-      data['username'] = this.state.formData.username;
-    }
-    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(res => {
-        this.clearFormState();
-        window.localStorage.setItem('authToken', res.auth_token);
-        this.setState({ isAuthenticated: true });
-        this.getUsers();
-      })
-      .catch(err => console.log(err))
-  }
-
-  clearFormState() {
-    this.setState({
-      formData: {username: '', email: '', password: ''},
-      username: '',
-      email: ''
-    });
-  }
-
-  handleFormChange(e) {
-    const { name, value } = e.target;
-    this.setState(prevState => ({
-      formData: {
-        ...prevState.formData,
-        [name]: value
-      }
-    }));
+  loginUser(token) {
+    window.localStorage.setItem('authToken', token);
+    this.setState({ isAuthenticated: true });
+    this.getUsers();
   }
 
   logoutUser() {
@@ -152,19 +70,15 @@ export default class App extends React.Component {
                   <Route exact path='/register' render={() => (
                     <Form
                       formType='Register'
-                      formData={this.state.formData}
-                      handleUserFormSubmit={this.handleUserFormSubmit}
-                      handleFormChange={this.handleFormChange}
                       isAuthenticated={this.state.isAuthenticated}
+                      loginUser={this.loginUser}
                     />
                   )} />
                   <Route exact path='/login' render={() => (
                     <Form
                       formType='Login'
-                      formData={this.state.formData}
-                      handleUserFormSubmit={this.handleUserFormSubmit}
-                      handleFormChange={this.handleFormChange}
                       isAuthenticated={this.state.isAuthenticated}
+                      loginUser={this.loginUser}
                     />
                   )} />
                   <Route exact path='/logout' render={() => (
--- services/client/src/components/Form.js
+++ services/client/src/components/Form.js
@@ -1,64 +1,140 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-const Form = props => {
-  if (props.isAuthenticated) {
-    return <Redirect to='/' />;
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    };
+
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
 
-  return (
-    <div>
-      {props.formType === 'Login' &&
-        <h1 className='title is-1'>Log In</h1>
+  componentDidMount() {
+    this.clearForm();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.formType !== nextProps.formType) {
+      this.clearForm();
+    }
+  }
+
+  clearForm() {
+    this.setState({
+      formData: {
+        username: '',
+        email: '',
+        password: ''
       }
-      {props.formType === 'Register' &&
-        <h1 className='title is-1'>Register</h1>
+    });
+  }
+
+  handleFormChange(e) {
+    const { name, value } = e.target;
+    this.setState(prev => ({
+      formData: {
+        ...prev.formData,
+        [name]: value
       }
-      <hr/><br/>
-      <form onSubmit={e => props.handleUserFormSubmit(e)}>
-        {props.formType === 'Register' &&
+    }));
+  }
+
+  handleUserFormSubmit(e) {
+    e.preventDefault();
+    const { formType, loginUser } = this.props;
+    const data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password
+    };
+    if (formType === 'Register') {
+      data.username = this.state.formData.username;
+    }
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType.toLowerCase()}`;
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.clearForm();
+        loginUser(res.auth_token);
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { username, email, password } = this.state.formData;
+    const { isAuthenticated, formType } = this.props;
+    if (isAuthenticated) {
+      return <Redirect to='/' />;
+    }
+
+    return (
+      <div>
+        {formType === 'Login' &&
+          <h1 className='title is-1'>Log In</h1>
+        }
+        {formType === 'Register' &&
+          <h1 className='title is-1'>Register</h1>
+        }
+        <hr/><br/>
+        <form onSubmit={e => this.handleUserFormSubmit(e)}>
+          {formType === 'Register' &&
+            <div className='field'>
+              <input
+                name='username'
+                className='input is-medium'
+                type='text'
+                placeholder='Enter a username'
+                required
+                value={ username }
+                onChange={ this.handleFormChange }
+              />
+            </div>
+          }
           <div className='field'>
             <input
-              name='username'
+              name='email'
               className='input is-medium'
-              type='text'
-              placeholder='Enter a username'
+              type='email'
+              placeholder='Enter an email address'
               required
-              value={props.formData.username}
-              onChange={props.handleFormChange}
+              value={ email }
+              onChange={ this.handleFormChange }
             />
           </div>
-        }
-        <div className='field'>
+          <div className='field'>
+            <input
+              name='password'
+              className='input is-medium'
+              type='password'
+              placeholder='Enter a password'
+              required
+              value={ password }
+              onChange={ this.handleFormChange }
+            />
+          </div>
           <input
-            name='email'
-            className='input is-medium'
-            type='email'
-            placeholder='Enter an email address'
-            required
-            value={props.formData.email}
-            onChange={props.handleFormChange}
+            type='submit'
+            className='button is-primary is-medium is-fullwidth'
+            value='Submit'
           />
-        </div>
-        <div className='field'>
-          <input
-            name='password'
-            className='input is-medium'
-            type='password'
-            placeholder='Enter a password'
-            required
-            value={props.formData.password}
-            onChange={props.handleFormChange}
-          />
-        </div>
-        <input
-          type='submit'
-          className='button is-primary is-medium is-fullwidth'
-          value='Submit'
-        />
-      </form>
-    </div>
-  );
-};
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Form;
